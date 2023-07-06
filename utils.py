db2227d7930cff42bd6f451c7d2b9a61dacf5b0e
Nishant Kumar <58890309+NishantKumar61988@users.noreply.github.com>
2023-07-06 18:04:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def query_refiner(conversation, query):
 
     response = openai.Completion.create(
     model= "text-davinci-003",
-    api_key="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",
+    api_key="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",
     prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
     temperature=0.7,
     max_tokens=256,
